d6aab7f5c25e5dff856f0911ca2d35698bb1a967
Gonzalo Rosano <rosanogonzalo@gmail.com>
2024-05-15 20:15:51 -0300

    Adicion de adapters para despliege en vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 import tailwind from "@astrojs/tailwind";
 import metaTags from "astro-meta-tags";
 import { defineConfig } from 'astro/config';
-
 import react from "@astrojs/react";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), metaTags(), react()],
-  output: "hybrid"
+  output: "hybrid",
+  adapter: vercel()
 });
